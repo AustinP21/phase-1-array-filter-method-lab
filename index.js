8bb708f7bf56ad9c04586e12333989be53e8350c
Austin <austinpulley19@yahoo.com>
2022-02-17 11:31:13 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers,originalName) {
+    const driverNames = drivers.filter(name => name.toLowerCase() === originalName.toLowerCase())
+    return driverNames
+}
+function fuzzyMatch(drivers,letters) {
+    const letterLength = letters.length
+    const startingLetters = drivers.filter(startLetters => startLetters.slice(0,letterLength) === letters)
+    return startingLetters
+}
+function matchName(drivers,matches) {
+    const nameMatch = drivers.filter(drivers => drivers.name === matches)
+    return nameMatch
+}
